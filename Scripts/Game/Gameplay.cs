e07eb8cbcaf1a7ee53903e2409bb1b20c0675f8f
夜轮_NachtgeistW <nachtgeistw@protonmail.com>
2024-01-06 20:42:10 +0800

    feat: 拆分note初始化与移动部分到NoteController

--- Scripts/Game/Gameplay.cs
+++ Scripts/Game/Gameplay.cs
@@ -4,22 +4,5 @@ using System;
 
 public partial class Gameplay : Node3D
 {
-    [Export] BlankNote blankNote;
-    [Export] HoldNote holdNote;
-
-    protected float chartPlaySpeed = 5f;
-
-    public override void _Ready()
-    {
-        base._Ready();
-        blankNote.Initialize();
-        holdNote.Initialize();
-    }
-
-    public override void _Process(double delta)
-    {
-        blankNote.NoteRenderer.Move(delta, chartPlaySpeed);
-        holdNote.NoteRenderer.Move(delta, chartPlaySpeed);
-    }
 
 }
---
+++ Scripts/Game/NoteController.cs
@@ -0,0 +1,63 @@
+﻿using Godot;
+using Plutono.Core.Note;
+using System.Collections.Generic;
+using Plutono.Util;
+
+public partial class NoteController : Node3D
+{
+    public List<BlankNote> BlankNotes = new();
+    public List<HoldNote> HoldNotes = new();
+
+    [Export] BlankNote blankNote;
+    [Export] HoldNote holdNote;
+
+    protected float chartPlaySpeed = 5f;
+
+    public override void _EnterTree()
+    {
+        base._EnterTree();
+
+        EventCenter.AddListener<FingerUpEvent>(OnFingerUp);
+    }
+
+    public override void _ExitTree()
+    {
+        base._ExitTree();
+
+        EventCenter.RemoveListener<FingerUpEvent>(OnFingerUp);
+    }
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        blankNote.Initialize();
+        BlankNotes.Add(blankNote);
+
+        holdNote.Initialize();
+        HoldNotes.Add(holdNote);
+    }
+
+    public override void _Process(double delta)
+    {
+        UpdateNotes(delta);
+    }
+
+    public void UpdateNotes(double delta)
+    {
+        foreach (var note in BlankNotes)
+        {
+            note.NoteRenderer.Move(delta, chartPlaySpeed);
+        }
+
+        foreach (var note in HoldNotes)
+        {
+            note.NoteRenderer.Move(delta, chartPlaySpeed);
+        }
+    }
+
+    private void OnFingerUp(FingerUpEvent @event)
+    {
+
+    }
+}
