23956b441f641e9ccc13b16d9dc26ebf02008fb8
夜轮_NachtgeistW <nachtgeistw@protonmail.com>
2024-08-06 11:17:49 +0800

    fix: the mouse motion detect

--- src/Scripts/Game/InputController.cs
+++ src/Scripts/Game/InputController.cs
@@ -8,6 +8,7 @@ public partial class InputController : Node
     [Export] private Game Game { get; set; }
     [Export] private TimeController TimeControl { get; set; }
 
+    private Vector2 lastPosition = Vector2.Inf;
 
     public override void _Input(InputEvent @event)
     {
@@ -17,34 +18,59 @@ public partial class InputController : Node
             {
                 if (eventMouseButton.IsPressed())
                 {
-                    Debug.Log("InputEventMouseButton Pressed");
+	                Debug.Log("InputEventMouseButton Pressed");
+	                OnFingerDown(eventMouseButton.Position);
                 }
                 else
                 {
                     Debug.Log("InputEventMouseButton Released");
-                    var pos = ScreenToWorldPoint(Game.OrthographicCamera, eventMouseButton.Position);
-                    EventCenter.Broadcast(new FingerDownEvent { WorldPos = pos, Time = TimeControl.CurTime });
+					OnFingerUp(eventMouseButton.Position);
                 }
-                Debug.Log("Mouse Click/Unclick at: ", eventMouseButton.Position);
+				Debug.Log("Mouse Click/Unclick at: ", eventMouseButton.Position);
                 Debug.Log(ScreenToWorldPoint(Game.OrthographicCamera, eventMouseButton.Position), " ", TimeControl.CurTime);
             }
         }
 
         if (@event is InputEventMouseMotion inputEventMouseMotion && inputEventMouseMotion.ButtonMask == MouseButtonMask.Left)
         {
-	        Debug.Log("InputEventMouseMotion Pressed");
-			var pos = ScreenToWorldPoint(Game.OrthographicCamera, inputEventMouseMotion.Position);
-	        EventCenter.Broadcast(new FingerMoveEvent { Finger = new Finger(), WorldPos = pos, Time = TimeControl.CurTime });
-		}
+	        if (lastPosition == Vector2.Inf)
+	        {
+		        lastPosition = inputEventMouseMotion.Position;
+	        }
+
+	        if ((inputEventMouseMotion.Position - lastPosition).Length() > 10f)
+	        {
+				Debug.Log($"InputEventMouseMotion Moved {(inputEventMouseMotion.Position - lastPosition).Length()}");
+				OnFingerMove(inputEventMouseMotion.Position);
+	        }
+        }
     }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="camera"></param>
-    /// <param name="screenPos"></param>
-    /// <returns>WorldPoint, or Vector3.Inf if worldPoint is null</returns>
-    private static Vector3 ScreenToWorldPoint(Camera3D camera, Vector2 screenPos)
+    private void OnFingerDown(Vector2 screenPos)
+    {
+	    var pos = ScreenToWorldPoint(Game.OrthographicCamera, screenPos);
+	    EventCenter.Broadcast(new FingerDownEvent { WorldPos = pos, Time = TimeControl.CurTime });
+    }
+
+    private void OnFingerMove(Vector2 screenPos)
+    {
+	    var pos = ScreenToWorldPoint(Game.OrthographicCamera, screenPos);
+	    EventCenter.Broadcast(new FingerMoveEvent { Finger = new Finger(), WorldPos = pos, Time = TimeControl.CurTime });
+    }
+
+    private void OnFingerUp(Vector2 screenPos)
+    {
+	    var pos = ScreenToWorldPoint(Game.OrthographicCamera, screenPos);
+	    EventCenter.Broadcast(new FingerUpEvent { WorldPos = pos, Time = TimeControl.CurTime });
+    }
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="camera"></param>
+	/// <param name="screenPos"></param>
+	/// <returns>WorldPoint, or Vector3.Inf if worldPoint is null</returns>
+	private static Vector3 ScreenToWorldPoint(Camera3D camera, Vector2 screenPos)
     {
         return camera.ProjectPosition(screenPos, camera.GlobalTransform.Origin.Z);
     }
