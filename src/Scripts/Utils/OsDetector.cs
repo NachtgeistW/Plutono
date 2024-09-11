8243ace1e7d8a345cbe50e67c6a957c5aaa6c89d
夜轮_NachtgeistW <nachtgeistw@protonmail.com>
2024-09-11 10:55:35 +0800

    feat: add OS Detector

---
+++ src/Scripts/Utils/OsDetector.cs
@@ -0,0 +1,33 @@
+﻿using Godot;
+
+namespace Plutono.Scripts.Utils;
+
+public partial class OsDetector : Node3D
+{
+	public static Platform Platform { get; private set; }
+
+	public override void _Ready()
+	{
+		base._Ready();
+
+		Platform = OS.GetName() switch
+		{
+			"Windows" or "macOS" or "Linux" or "FreeBSD" or "NetBSD" or "OpenBSD" or "BSD" => Platform.PC,
+			"Android" => Platform.Android,
+			"iOS" => Platform.iOS,
+			"Web" => Platform.Web,
+			_ => Platform.GenericDevice
+		};
+	}
+}
+
+public enum Platform
+{
+	// ReSharper disable once InconsistentNaming
+	PC,
+	Android,
+	// ReSharper disable once InconsistentNaming
+	iOS,
+	Web,
+	GenericDevice
+}
---
+++ src/Scripts/Utils/Singleton.cs
@@ -0,0 +1,44 @@
+﻿//using Godot;
+
+//public class Singleton<T> : Node where T : Singleton<T>
+//{
+//	private static T _instance;
+//	public static T Instance
+//	{
+//		get
+//		{
+//			if (_instance == null)
+//			{
+//				_instance = (T)FindObjectOfType(typeof(T));
+//				if (_instance == null)
+//				{
+//					SetupInstance();
+//				}
+//			}
+//			return _instance;
+//		}
+//	}
+
+//	protected virtual void Awake()
+//	{
+//		if (_instance != null)
+//			QueueFree();
+//		else
+//		{
+//			_instance = this as T;
+//		}
+//	}
+
+//	private static void SetupInstance()
+//	{
+//		_instance = (T)FindObjectOfType(typeof(T));
+//		if (_instance == null)
+//		{
+//			var obj = new Node
+//			{
+//				Name = typeof(T).Name
+//			};
+//			_instance = obj.AddComponent<T>();
+//		}
+//	}
+//}
